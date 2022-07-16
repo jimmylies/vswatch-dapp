eb6f18641dbc008792582aefeb46209ca44cc028
thibz <tybernox@gmail.com>
2022-07-16 14:16:35 +0200

    feat: add contact in footer

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -11,6 +11,9 @@ const Footer = ({ category }) => {
             The alliance between the technology of the future and a timeless
             creation
           </p>
+          <p>
+            Contact us at: <span className='underline'>vswatch@gmail.com</span>
+          </p>
           <p className='copyright'>©2022 VSWatch NFT - All rights reserved</p>
         </>
       ) : (
@@ -18,6 +21,10 @@ const Footer = ({ category }) => {
           <p className='catchphrase'>
             L’alliance entre la technologie du futur et une création
             intemporelle.
+          </p>
+          <p>
+            Contactez nous ici:{' '}
+            <span className='underline'>vswatch@gmail.com</span>
           </p>
           <p className='copyright'>©2022 VSWatch NFT - Tous droits réservés</p>
         </>
